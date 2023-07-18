8bbf1c4f58351b3e826bfc3210d15f5a88e7c3bf
MinhSang <sang.nguyen@atech88.com>
2023-07-18 10:51:57 +0700

    Description Cart API

--- Sales_Website_Ecomerce/src/WebApp/Clients/Client.API/Controllers/V1/CartController.cs
+++ Sales_Website_Ecomerce/src/WebApp/Clients/Client.API/Controllers/V1/CartController.cs
@@ -18,6 +18,12 @@ namespace Client.API.Controllers
             _cartService = cartService;
         }
 
+        /// <summary>
+        /// hiển thị cart theo mã KH
+        /// </summary>
+        /// <param name="CustomerID"></param>
+        /// <param name="pageIndex"></param>
+        /// <returns></returns>
         [HttpGet("/FindCart/{CustomerID}")]
         public ActionResult<ProductResponeModel> Get([Required] Guid CustomerID, [Required] int pageIndex)
         {
@@ -30,27 +36,50 @@ namespace Client.API.Controllers
         ////    return Ok(_productService.GetAll(pageIndex));
         ////}
 
+        /// <summary>
+        /// thêm sản phẩm vào giỏ hàng (nếu KH đã có giỏ hàng sẽ hiển thị thông báo, nếu KH đồng ý gọi API: AddProductExisted)
+        /// </summary>
+        /// <param name="cart"></param>
+        /// <returns></returns>
         [HttpPost("/AddCart")]
         public ActionResult AddCart([FromBody][Required] CartRequestModel cart)
         {
             return Ok(_cartService.Create(cart));
         }
 
-        [HttpPost("/AddProductExisted")]// khi giỏ hang đã có product
+        /// <summary>
+        /// thêm sản phẩm khi KH đã có giỏ hàng
+        /// </summary>
+        /// <param name="cart"></param>
+        /// <returns></returns>
+        [HttpPost("/AddProductExisted")]
         public ActionResult AddProductExisted([FromBody][Required] CartRequestModel cart)
         {
             return Ok(_cartService.AddProductExisted(cart));
         }
 
-        //update 
-            //+ chỉ xóa 1 cartproduct từ cart và k xóa cart
-            //+ update số lượng
+        /// <summary>
+        /// UpdateCart
+        /// </summary>
+        /// <remarks>
+        /// Update:
+        /// + chỉ xóa 1 cartproduct từ cart và k xóa cart (xóa thì gọi API: DeleteCart)
+        /// + update số lượng
+        /// </remarks>
+        /// <param name="item"></param>
+        /// <param name="cartID"></param>
+        /// <returns></returns>
         [HttpPut("/UpdateCart")]
         public ActionResult UpdateCart([FromBody][Required] CartRequestModel item, [Required] Guid cartID)
         {
             return Ok(_cartService.Update(item, cartID));
         }
-        //xóa là update cart và cartproduct
+
+        /// <summary>
+        /// DeleteCart (update cart và cartproduct)
+        /// </summary>
+        /// <param name="cartID"></param>
+        /// <returns></returns>
         [HttpDelete("/DeleteCart")]
         public ActionResult DeleteCart([Required] Guid cartID)
         {
